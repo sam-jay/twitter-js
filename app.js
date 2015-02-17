5473c2dcf81011a9db4376c52c77cca4e38258b6
Samurdha Jayasinghe <sj2564@columbia.edu>
2015-02-17 12:38:44 -0500

    added swig

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 (function() {
 	var express = require('express'),
-		morgan = require('morgan');
+		morgan = require('morgan'),
+		swig = require('swig');
 
 	var app = express();
 
 	app.use(morgan('dev'));
 
+	app.engine('html', swig.renderFile);
+	app.set('view engine', 'html');
+	app.set('views', process.cwd() + '/views');
+
+	swig.setDefaults({ cache: false });
+
+	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 	app.get('/', function(req, res) {
-		res.send('Hello World!');
+		res.render( 'index', {title: 'Hall of Fame', people: people} );
 	});
 
 
